61e1f191df3363a5dc80dbf92991c6eed34c69ff
Tahsin Gungordu <tahsingungordu@gmail.com>
2017-04-07 02:59:13 +0300

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :users, except: [:show]
 
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "users#index"
 end
