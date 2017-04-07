ee2923d289d7cf5b532a64505fb52377d90d6764
Tahsin Gungordu <tahsingungordu@gmail.com>
2017-04-07 03:13:19 +0300

    creating view and controller for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path, notice: "The user has been created!" and return
+    end
+    render 'new'
   end
 
   def edit
@@ -16,5 +23,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :notes)
   end
 end
